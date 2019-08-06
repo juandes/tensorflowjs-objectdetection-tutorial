5a08afa3c76d4ee062229cd031b3cb2fe08742be
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2019-08-06 20:57:35 +0700

    Changed font size in detect.js

--- detect.js
+++ detect.js
@@ -6,8 +6,8 @@ function detect(url) {
   const img = new Image();
   img.crossOrigin = "anonymous";
   img.src = url;
-  img.width = 224;
-  img.height = 224;
+  img.width = 400;
+  img.height = 400;
 
   console.log(url)
 
@@ -20,23 +20,25 @@ function detect(url) {
 
   const canvas = document.getElementById("myCanvas");
   const ctx = canvas.getContext("2d");
-  canvas.width  = 500;
-  canvas.height = 500;
+  canvas.width  = 1000;
+  canvas.height = 1000;
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  const font = "24px helvetica";
+  ctx.font = font;
+  ctx.textBaseline = "top";
   
-  //ctx.drawImage(img,0, 0,300,300);
+  ctx.fillText("Predicting...", 50, 50)
 
   // Load the model.
   cocoSsd.load().then(model => {
     console.log('Predicting')
 
-    ctx.drawImage(img,0, 0,224,224);
+    ctx.drawImage(img,0, 0,400,400);
     // detect objects in the image.
     model.detect(img).then(predictions => {
       predictions.forEach(prediction => {
-        const font = "14px helvetica";
-        ctx.font = font;
-        ctx.textBaseline = "top";
+        
         
         const x = prediction.bbox[0];
         const y = prediction.bbox[1];
@@ -50,8 +52,10 @@ function detect(url) {
         ctx.fillStyle = "#2fff00";
         const textWidth = ctx.measureText(prediction.class).width;
         const textHeight = parseInt(font, 10); // base 10
-        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-        ctx.fillRect(x, height - textHeight + 4, textWidth + 4, textHeight + 4);
+        // top left rectangle
+        ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
+        // bottom left rectangle
+        ctx.fillRect(x, height - textHeight + 10, textWidth + 15, textHeight + 10);
     });
 
       predictions.forEach(prediction => {
@@ -62,7 +66,7 @@ function detect(url) {
         // Draw the text last to ensure it's on top.
         ctx.fillStyle = "#000000";
         ctx.fillText(prediction.class, x, y);
-        ctx.fillText(prediction.score, x, height);
+        ctx.fillText(prediction.score.toFixed(2), x, height - 4);
       });
       
       console.log('Predictions: ', predictions);
